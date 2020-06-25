a8c4725e8ee6ff5f7889eb2f55e2e2054bed2f1d
Rafael Vasconcelos <rafaelvasconcelos@MacBook-Pro-de-Rafael.local>
2020-06-24 21:30:14 -0300

    Initial commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../reporsitories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  response.json(await appointmentsRepository.find());
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+    const appointment = await createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
